e43df11a42053a14eac2d8b152c23bc52765d8e0
Jones <doc.stetsko@gmail.com>
2023-06-22 11:34:54 +0200

    Delete commented code

--- main.py
+++ main.py
@@ -38,15 +38,7 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
-    # Detect collision with the tail (ver_01)
-    # for segment in snake.segments:
-    #     if segment == snake.head:
-    #         pass
-    #     elif snake.head.distance(segment) < 10:
-    #         game_is_on = False
-    #         scoreboard.game_over()
-
-    # Detect collision with the tail (ver_02)
+    # Detect collision with the tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
